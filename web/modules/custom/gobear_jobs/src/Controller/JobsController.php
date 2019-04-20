514b3bf38fb5453c80c6f8b8e7818dfc19139405
Tu Phan <tuphan@globalinnovation.co>
2019-04-20 17:01:43 +0700

    assessment

--- web/modules/custom/gobear_jobs/src/Controller/JobsController.php
+++ web/modules/custom/gobear_jobs/src/Controller/JobsController.php
@@ -4,11 +4,10 @@
  * @file
  * Contains \Drupal\gobear_jobs\Controller\LikeController.
  */
-
 namespace Drupal\gobear_jobs\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
 use Drupal\Component\Serialization\Json;
+use Drupal\Core\Controller\ControllerBase;
 
 /**
  * Class LikeController.
@@ -17,47 +16,21 @@ use Drupal\Component\Serialization\Json;
  */
 class JobsController extends ControllerBase {
 
+  /**
+   * @return mixed
+   */
   public function jobsListing() {
 
-    /*$storage = \Drupal::getContainer()->get('entity_type.manager')->getStorage('node');
-    $nids = $storage->getQuery()->condition('type', 'article')
-      ->pager(10)
-      ->sort('changed', 'DESC')
-      ->execute();
-
-    $items = [];
-    foreach ($nids as $nid) {
-      $node_load = Node::load($nid);
-      $items[] = [
-        'title' => Link::fromTextAndUrl(t($node_load->getTitle()), Url::fromUri('internal:/node/' . $nid)),
-        'body' => text_summary($node_load->body->value),
-      ];
-    }
-
-    $render[] =  [
-      '#items' => $items,
-      '#theme' => 'recent_article',
-      '#attached' => [
-        'library' => [
-          'recent/recent',
-        ],
-      ],
-    ];
-
-    $render[] = [
-      '#type' => 'pager',
-    ];*/
-
-  $uri = "https://jobs.github.com/positions.json?location=new+york";
-   try {
-      $response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'application/json')));
+    $uri = 'https://jobs.github.com/positions.json?location=new+york';
+    try {
+      $response = \Drupal::httpClient()->get($uri, ['headers' => ['Accept' => 'application/json']]);
       $data = (string) $response->getBody();
       if (empty($data)) {
         return FALSE;
-      }else{
+      } else {
         $jobs = Json::decode($data);
 
-        $render[] =  [
+        $render[] = [
           '#items' => $jobs,
           '#theme' => 'jobs_listing',
           '#attached' => [
@@ -66,15 +39,14 @@ class JobsController extends ControllerBase {
             ],
           ],
         ];
+
         return $render;
         //ksm($json);
       }
-    }
-    catch (RequestException $e) {
-        return FALSE;
+    } catch (RequestException $e) {
+      return FALSE;
     }
 
     return FALSE;
   }
-
 }
